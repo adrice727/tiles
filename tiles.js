5380c7466561537b1fcc21e55271ac0063332d14
Aaron Rice <adrice727@gmail.com>
2017-11-30 16:25:58 -0800

    Add adjustments.

--- tiles.js
+++ tiles.js
@@ -5,11 +5,15 @@ const runTiles = () => {
     const el = document.createElement('div');
     el.id = 'tiles';
     el.style.width = '100vw';
+    el.style.position = 'absolute';
+    el.style.top = '0px';
     return el;
   })();
 
-  const TILES_PER_ROW = 50;
-  const TOTAL_ROWS = 30;
+  const TILES_PER_ROW = 100;
+  const TOTAL_ROWS = 80;
+  const COLOR_CONCENTRATION = .60;
+  const MAX_OPACITY = .50;
   const TOTAL_TILES = TILES_PER_ROW * TOTAL_ROWS;
   let tileSize;
   const tileRows = [];
@@ -29,13 +33,13 @@ const runTiles = () => {
 
   const randomColor = (row, column) => {
     const hasColor =
-      (Math.random() * (1.0 - (.50 * (((row + 1) * (column + 1) / TOTAL_TILES))))) > .5;
+      (Math.random() * (COLOR_CONCENTRATION - (.65 * (((row + 1) * (column + 1) / TOTAL_TILES))))) > .5;
     return hasColor ? colors[(Math.random() * colors.length) | 0] : 'white';
   };
 
   const randomOpacity = (row, column) => {
     const adjustment = ((Math.random() * 30) * ((row + 1) * (column + 1)) / TOTAL_TILES);
-    return .45 - adjustment;
+    return MAX_OPACITY - adjustment;
   }
   const buildTile = (row, column) => {
     const el = document.createElement('div');
@@ -45,7 +49,7 @@ const runTiles = () => {
     el.style.height = tileSize;
     el.style.width = tileSize;
     el.style.float = 'left';
-    el.style.transition = 'all 2.0s cubic-bezier(0.47, 0, 0.75, 0.72)';
+    el.style.transition = 'all 2.0s ease';
     return el;
   };
 
@@ -70,10 +74,10 @@ const runTiles = () => {
         tile.style.opacity = randomOpacity(rowIndex, columnIndex);
       })
     })
-  }
+  };
 
   buildTiles();
   setInterval(updateColors, 4500);
-}
+};
 
 document.addEventListener('DOMContentLoaded', runTiles);
